a977d309bc44657721f7e2bebd8b65e8d48fe9a3
NDNCI <contact@ndnci.com>
2020-06-10 21:40:33 +0200

    assets dir added and gulp config changed

---
+++ templates/classic/assets/js/script.js
@@ -0,0 +1,7 @@
+class Test{
+
+    hello() {
+        return "Hello World";
+    }
+    
+}
--- templates/classic/gulpfile.js
+++ templates/classic/gulpfile.js
@@ -2,75 +2,132 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
 const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 
 // OPTIONS
-var autoprefixerOptions = {
-    // overrideBrowserslist: ['last 10 versions'] // already written in package.json
+const options = {
+    // CSS
+    css: {
+        src: './assets/css/**/*.css',
+        concat: 'index.css',
+        dest: './dist/css'
+    },
+    // SASS
+    sass: {
+        src: './assets/scss/**/*.scss',
+        concat: 'index.css',
+        dest: './dist/scss'
+    },
+    // JAVASCRIPT
+    javascript: {
+        src: './assets/js/**/*.js',
+        concat: 'index.js',
+        dest: './dist/js'
+    },
+    // AUTOPREFIXER
+    autoprefixer: {
+        overrideBrowserslist: ['last 10 versions']
+    },
+    // BABEL
+    babel: {
+        presets: ['@babel/env']
+    },
+    // BROWSER SYNC
+    browserSync: {
+        active: false,
+        baseDir: './',
+        watch: './**/*.html'
+    }
 };
 
-// style files compiler
-function style() {
-    // 1. where is my scss file
-    return gulp.src('./assets/scss/**/*.scss')
+// css files compiler
+function _css(done) {
+    // 1. where is css files
+        gulp.src(options.css.src)
+    // 2 init sourcemaps
+        .pipe(sourcemaps.init())
+    // 2.1.1 autoprefixer filter
+        .pipe(autoprefixer(options.autoprefixer))
+    // 2.2 concat files with custom name
+        .pipe(concat(options.css.concat))
+    // 2.3 write sourcemaps file
+        .pipe(sourcemaps.write())
+    // 3. where do i save the compiled CSS ?
+        .pipe(gulp.dest(options.css.dest))
+    // 4. stream changes to all browser
+        .pipe(browserSync.stream())
+
+        done();
+};
+
+// sass files compiler
+function _scss(done) {
+    // 1. where is scss files
+        gulp.src(options.sass.src)
     // 2 init sourcemaps
         .pipe(sourcemaps.init())
     // 2.1 pass that file through sass compiler
         .pipe(sass())
     // 2.1.1 autoprefixer filter
-        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(autoprefixer(options.autoprefixer))
     // 2.2 concat files with custom name
-        .pipe(concat('mentor.css'))
+        .pipe(concat(options.sass.concat))
     // 2.3 write sourcemaps file
         .pipe(sourcemaps.write())
     // 3. where do i save the compiled CSS ?
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest(options.sass.dest))
     // 4. stream changes to all browser
-        .pipe(browserSync.stream());
-}
+        .pipe(browserSync.stream())
+
+        done();
+};
 
 // javascript files compiler
-function javascript() {
-    // 1. where is my js file
-    return gulp.src('./assets/js/**/*.js')
+function _js(done) {
+    // 1. where is js files
+    gulp.src(options.javascript.src)
     // 2 init sourcemaps
         .pipe(sourcemaps.init())
     // 2.1 pass that file through babel compiler
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
+        .pipe(babel(options.babel))
     // 2.2 concat files with custom name
-        .pipe(concat('mentor.js'))
+        .pipe(concat(options.javascript.concat))
     // 2.3 write sourcemaps file
         .pipe(sourcemaps.write())
     // 3. where do i save the compiled JS ?
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest(options.javascript.dest))
     // 4. stream changes to all browser
-        .pipe(browserSync.stream());
-}
+        .pipe(browserSync.stream())
+
+        done();
+};
 
 // gulp watcher
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    // listening all scss files in assets/scss directory
-    gulp.watch('./assets/scss/**/*.scss', style);
-    // listening all js files in assets/js directory
-    gulp.watch('./assets/js/**/*.js', javascript);
-    // listening all html files in root directory
-    gulp.watch('./**/*.html').on('change', browserSync.reload);
-}
+function _watch(done) {
+
+    if(options.browserSync.active) {
+
+        // if browserSync is activated
+        browserSync.init({
+            server: {
+                baseDir: options.browserSync.baseDir
+            }
+        });
+
+        // listening specific files directory
+        gulp.watch(options.browserSync.watch).on('change', browserSync.reload);
+    }
+
+    // listening all scss files
+    gulp.watch(options.sass.src, _scss);
+    // listening all js files
+    gulp.watch(options.javascript.src, _js);
+
+    done();
+};
 
 // default gulp function for using only "gulp" on terminal
-gulp.task('default', () =>
-    watch()
-);
-
-exports.style = style;
-exports.javascript = javascript;
-exports.watch = watch;
+exports.default = gulp.series(_css, _scss, _js);
+exports.watch = _watch;
